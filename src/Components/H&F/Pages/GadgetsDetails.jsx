ac420fad02b3b468eeda818d2b8d89478becb270
mohammadamzad38 <hossainamzad38@gmail.com>
2024-12-29 11:17:56 +0600

    spec update

--- src/Components/H&F/Pages/GadgetsDetails.jsx
+++ src/Components/H&F/Pages/GadgetsDetails.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useLoaderData, useParams } from "react-router-dom";
 import { MdOutlineShoppingCart } from "react-icons/md";
 import { FaRegHeart } from "react-icons/fa6";
+import { key } from "localforage";
 
 const GadgetsDetails = () => {
   const { product_id } = useParams();
@@ -16,6 +17,9 @@ const GadgetsDetails = () => {
     rating,
     description,
   } = gadget;
+
+  const customArray = Object.entries(specification).map(([key, value]) => ({key,value}));
+  console.log('arrrrrrrrrrrr',customArray)
   return (
     <section>
       <div className="bg-[#9538E2] pt-10 pb-[250px] space-y-4 text-white">
@@ -44,7 +48,10 @@ const GadgetsDetails = () => {
               </h1>
             </div>
             <p>{description}</p>
-            {/* <p>Specification: {specification}</p> */}
+            <p className="font-bold">Specification:</p>
+            {
+              customArray.map((spec) => <p className="ml-10">{spec.key}: {spec.value}</p>)
+            }
             <p className="font-bold">Ratings: {rating}</p>
             <div className="rating space-x-3">
               <input
